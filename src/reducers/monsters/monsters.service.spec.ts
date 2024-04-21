fcc1224ae26fc85c085d014ecfe9e2ed6f069c17
Angel <jose.prieto@levva.io>
2024-04-21 19:58:02 -0300

    test: service battle result

--- src/reducers/monsters/monsters.service.spec.ts
+++ src/reducers/monsters/monsters.service.spec.ts
@@ -2,6 +2,10 @@ import { MonsterService } from './monsters.service';
 import monstersData from '../../../server/monsters.json';
 
 describe('Monsters Service', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
   it('should return the monsters list empty', async () => {
     jest.spyOn(MonsterService, 'getAll').mockResolvedValue([]);
     const response = await MonsterService.getAll();
@@ -17,5 +21,19 @@ describe('Monsters Service', () => {
       monstersData.monsters[0],
       monstersData.monsters[1],
     ]);
+  });
+
+  it('should successfully retrieve battle results', async () => {
+    const mockResult = { winner: monstersData.monsters[0], tie: false };
+
+    const battleData = {
+      selectedMonsterId: monstersData.monsters[0].id,
+      opponentMonsterId: monstersData.monsters[1].id,
+    };
+
+    jest.spyOn(MonsterService, 'resultBattle').mockResolvedValue(mockResult);
+
+    const response = await MonsterService.resultBattle(battleData);
+    expect(response).toEqual(mockResult);
   });
 });
