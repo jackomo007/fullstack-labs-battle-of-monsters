11715e7f3ad31689fa5f5981c762b23cc23a1869
Angel <jose.prieto@levva.io>
2024-04-21 16:01:51 -0300

    add: selector to obtain battle result

--- src/reducers/monsters/monsters.selectors.ts
+++ src/reducers/monsters/monsters.selectors.ts
@@ -7,3 +7,5 @@ export const selectSelectedMonster = (state: RootState) =>
 
 export const setComputerMonster = (state: RootState) =>
   state.monsters.computerMonster;
+
+export const returnResult = (state: RootState) => state.monsters.result;
